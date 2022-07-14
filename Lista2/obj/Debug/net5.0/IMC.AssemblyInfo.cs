8593436cdaae372284b72746deada3180d0d6020
raul-andres-martinez <rams8@outlook.com>
2022-07-14 16:40:32 -0300

    Enviando lista 2 IMC 2

--- Lista2/obj/Debug/net5.0/IMC.AssemblyInfo.cs
+++ Lista2/obj/Debug/net5.0/IMC.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("IMC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("HelloWorld2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld2")]
+[assembly: System.Reflection.AssemblyProductAttribute("IMC")]
+[assembly: System.Reflection.AssemblyTitleAttribute("IMC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
---
+++ Lista3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            int age = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite seu peso: ");
+            float weight = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite sua altura: ");
+            float height = float.Parse(Console.ReadLine());
+
+            float imc = weight / (height * height);
+
+            Console.WriteLine("\r\nOi, " + name);
+            Console.WriteLine("\r\nVocê tem " + age);
+            Console.WriteLine("\r\nVocê pesa " + weight + "kg");
+            Console.WriteLine("\r\nVocê mede " + height + "m");
+            Console.WriteLine("\r\nSeu imc é " + Math.Round(imc, 2));
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Você está abaixo do peso.");
+            }
+            else if (imc >= 18.5 && imc <= 24.9) 
+            {
+                Console.WriteLine("Você está com o peso normal");
+            }
+            else if (imc >= 25 && imc <= 29.9){
+                Console.WriteLine("Você está acima do peso.");
+            }
+            else if (imc >= 30 && imc <= 34.9){
+                Console.WriteLine("Você está com obesidade grau 1");
+            }
+            else if (imc >= 35 && imc <= 39.9){
+                Console.WriteLine("Você está com obesidade grau 2");
+            }
+            else if (imc >= 40){
+                Console.WriteLine("Você está com obesidade mórbida");
+            }
+        }
+    }
+}
