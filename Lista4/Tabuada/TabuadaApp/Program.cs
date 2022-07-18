92b9ba6bf39b62c0c9dc607555975d084e52a89d
raul-andres-martinez <rams8@outlook.com>
2022-07-18 09:53:58 -0300

    Tabuada

---
+++ Lista4/Tabuada/TabuadaApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TabuadaApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            Console.WriteLine("Informe o número para a tabuada: ");
+            int number = Int32.Parse(Console.ReadLine());
+
+            for (x = 1; x <= 10; x++)
+            {
+                int result = number * x;
+                Console.WriteLine("\r\n" + number + " X " + x + " = " + result);
+
+            } 
+
+        }
+    }
+}
