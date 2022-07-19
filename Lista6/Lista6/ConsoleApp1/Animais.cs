3716e281de26ed135a20a8325a92408793443c6b
raul-andres-martinez <rams8@outlook.com>
2022-07-19 16:50:22 -0300

    Lista 6

---
+++ Lista6/Lista6/ConsoleApp1/Animais.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Animais
+    {
+        public Categoria Classe { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Especie { get; set; }
+
+        public int Idade { get; set; }
+
+        public Animais(Categoria classe, string nome, string especie, int idade)
+        {
+            Classe = classe;
+            Nome = nome;
+            Especie = especie;
+            Idade = idade;
+        }
+
+        public override string ToString()
+        {
+            return $"\nNome do animal: {Nome}" +
+                   $"\nClasse: {Classe}" +
+                   $"\nEspécie: {Especie}" +
+                   $"\nIdade: {Idade}";
+        }
+    }
+
+
+    public enum Categoria
+    {
+        REPTIL,
+        MAMIFERO,
+        ANFIBIO,
+    }
+}
---
+++ Lista6/Lista6/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animais gato = new Animais(Categoria.MAMIFERO, "Erin", "Gato", 2);
+            Animais iguana = new Animais(Categoria.REPTIL, "Ana", "Iguana", 7);
+            Animais tartaruga = new Animais(Categoria.ANFIBIO, "Leonardo", "Tartaruga", 1);
+            Animais cachorro = new Animais(Categoria.MAMIFERO, "Rex", "Cachorro", 6);
+
+            Console.WriteLine(gato.ToString());
+            Console.WriteLine(iguana.ToString());
+            Console.WriteLine(tartaruga.ToString());
+            Console.WriteLine(cachorro.ToString());
+        }
+    }
+}
